d590ec4cb670987901259f2f6a7c306dc2438e64
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-02-13 18:12:05 +0000

    created components

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import ToTop from "./components/ToTop";
+export default function App(){
+  return(
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/components/MainTitle.jsx
@@ -0,0 +1,8 @@
+export default function MainTitle({title, secondTitle}) {
+    return(
+        <div>
+            <h1>{title}</h1>
+            <h2>{secondTitle}</h2>
+        </div>
+    )
+}
