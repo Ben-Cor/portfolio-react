bd6185bfe0088ead41d437b795bb1da02265b15f
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-02-21 10:59:30 +0000

    further style amends

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -5,9 +5,10 @@ import Button from "./atoms/Button";
 
 export default function Career() {
     return (
-        <div className="flex flex-col items-end text-right text-slate-800 pr-[5%] py-[15vh] md:py-[20vh]">
+        <div className="flex flex-col items-end text-right text-slate-800 max-w-[95%] pr-[3%] py-[15vh] md:py-[20vh]">
             <SectionTitle title={"Career"} />
             <div className="flex flex-col-reverse items-center justify-start gap-[10%] md:flex-row md:w-[100%]">
+                <Images className={"w-[300px] mt-20 md:w-[30vw] md:mt-0"} src={"src/images/careerGeo2.png"} alt={'Geometric shapes with a graduation cap shown'} />
                 <TextBodyRight text={`I have been working as an Architect and Senior Architect 
                     since 2015 where I have been responsible for managing both teams of architects and 
                     a range of projects of values up to £4 million. I have obtained a variety of qualifications 
@@ -15,7 +16,6 @@ export default function Career() {
                     diploma in Architectural Professional Practice. In additon, I have been ARB chartered and a 
                     member of the RIBA. Recently, I have also obtained a NCFE Level 2 Certificate in 
                     Understanding Coding. A copy of my C.V. is available to download using the link below.`} />
-                <Images className={"w-[300px] mt-20 md:w-[30vw] md:mt-0"} src={"src/images/careerGeo2.png"} alt={'Geometric shapes with a graduation cap shown'} />
             </div>
             <Button text={'View my C.V.'} />
         </div>
