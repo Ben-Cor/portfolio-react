7645c769b6d417a6f50a1553c30c4cf0989a7359
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-02-24 08:33:39 +0000

    initial content added

--- src/components/Coding.jsx
+++ src/components/Coding.jsx
@@ -1,7 +1,17 @@
 export default function Coding() {
     return (
         <div>
-            
+            <div className="flex flex-col items-start text-left pb-[22vh] pt-[17vh] pl-[5%] md:pb-[27vh] md:pt-[17vh]">
+                        <SectionTitle title={"Coding"} />
+                        <div className="flex flex-col items-start justify-start gap-[10%] w-[95%] md:items-center md:flex-row md:justify-start">
+                            <TextBodyLeft text={`During my iO academy bootcamp and throughout my own experiences, I have used a variety of programming and
+                                markup languages such as <strong class="text-2xl mx-1 font-normal">HTML, CSS, JavaScript, MySQL and PHP.
+                                In additon, I have utilised several frameworks and libraries such as Tailwind and React.  
+                                While undertaking a distance learning course in order to obtain a NCFE Level 2 Certificate in Understanding Coding, 
+                                I learnt about the development lifecycle as well as best practice and technical terminology.
+                                Below you can see some of the projects I have developed.`} />
+                        </div>
+                    </div>
         </div>
     )
 }
