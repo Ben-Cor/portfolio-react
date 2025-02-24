3eb9ae972ec08a39b6d5e6ed893df41a10385095
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-02-24 16:11:59 +0000

    json placeholder and title added

--- src/components/Coding.jsx
+++ src/components/Coding.jsx
@@ -1,3 +1,7 @@
+import SectionTitle from "./atoms/SectionTitle";
+import TextBodyLeft from "./atoms/TextBodyLeft";
+
+
 export default function Coding() {
     return (
         <div>
@@ -5,13 +9,19 @@ export default function Coding() {
                         <SectionTitle title={"Coding"} />
                         <div className="flex flex-col items-start justify-start gap-[10%] w-[95%] md:items-center md:flex-row md:justify-start">
                             <TextBodyLeft text={`During my iO academy bootcamp and throughout my own experiences, I have used a variety of programming and
-                                markup languages such as <strong class="text-2xl mx-1 font-normal">HTML, CSS, JavaScript, MySQL and PHP.
+                                markup languages such as HTML, CSS, JavaScript, SQL, and PHP.
                                 In additon, I have utilised several frameworks and libraries such as Tailwind and React.  
                                 While undertaking a distance learning course in order to obtain a NCFE Level 2 Certificate in Understanding Coding, 
                                 I learnt about the development lifecycle as well as best practice and technical terminology.
                                 Below you can see some of the projects I have developed.`} />
                         </div>
-                    </div>
+                        <div class="py-[5%] max-w-[90%]">
+                            <h6 class="md:text-4xl text-2xl my-[5%]">Coding Projects</h6>
+                        <div id="projectGrid" class="geoimageRight flex flex-wrap items-center justify-start gap-[5%] md:gap-20 my-2">
+                    
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
