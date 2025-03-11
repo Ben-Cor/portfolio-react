624cdceb546b0908d11f5a9a368146a5836888ed
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-03-11 08:47:59 +0000

    flashing cursor added

--- src/components/atoms/SectionTitle.jsx
+++ src/components/atoms/SectionTitle.jsx
@@ -1,25 +1,10 @@
-// import { useEffect, useState } from "react"
-
 export default function SectionTitle({title}){
-
-// const [isBlinking, setIsBlinking] = useState(true);
-
-// const titleWithCursor = {title};
-
-// function cursorBlinking(){
-//     titleWithCursor += "|";
-//     delay (removeCursor, 80);
-// }
-
-// function removeCursor(){
-//     {text}substring(0, val.length - 1);
-// }
-
-    // useEffect([]);
 
     return(
         <div>
-            <h3 className="pt-10 text-[4rem] font-primary mb-[5%] md:mb-0 md:text-[8rem] md:pt-0">{title}</h3>
+            <h3 className="pt-10 text-[4rem] font-primary mb-[5%] md:mb-0 md:text-[8rem] md:pt-0">{title}
+                <span className="animate-blink border-r-[5px] border-slate-900 pl-3"></span>
+            </h3>
         </div>
     )
 }
