0aeeb08044db0621b975c5bcf30688454c3c9352
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-03-12 13:35:38 +0000

    content and image changed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,12 +7,11 @@ export default function About() {
         <div id="about" className="flex flex-col items-start text-left pb-[22vh] pt-[17vh] pl-[5%] md:pb-[27vh] md:pt-[17vh]">
             <SectionTitle className="" title={"About"} />
             <div className="flex flex-col items-start justify-start gap-[10%] w-[95%] md:items-start md:flex-row md:justify-start">
-                <TextBodyLeft text={`I'm an architect & an aspiring Software Engineer.
+                <TextBodyLeft text={`I'm an aspiring Software Engineer working with a range of languages and frameworks 
+                        including React, MySQL, Laravel, and PHP.
                         I have always had an interest in coding since editing a MySpace page when I was young, and recently I have had the 
-                        opportunity to greatly expand on these skills through a software development bootcamp.
-                        My architectural background has given me good communication skills, problem solving abilities, and the ability to 
-                        collaborate well on projects`} />
-                <Images className={"w-[300px] mt-5 md:w-[30vw] md:pr-0"} src={"src/images/aboutGeo2.png"} alt={"Geometric shapes with a lightbulb"} />
+                        opportunity to greatly expand on these skills through a software development bootcamp.`} />
+                <Images className={"w-[300px] mt-5 border-slate-900 border-2 shadow-2xl md:w-[30vw] md:pr-0"} src={"src/images/typingGIF.gif"} alt={"A gif showing Jim Carey typing manically"} />
             </div>
         </div>
     )
