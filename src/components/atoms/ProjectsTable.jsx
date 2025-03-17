e9304a3f3d23749dbe8c583c8261235801eee964
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-03-17 11:57:36 +0000

    commented code removed

--- src/components/atoms/ProjectsTable.jsx
+++ src/components/atoms/ProjectsTable.jsx
@@ -59,40 +59,4 @@ export default function ProjectsTable() {
       </div>
     </div>
   );
-}
-
-
-  //   return (
-  //     <div className="grid grid-cols-1 gap-10 my-2 font-secondary md:flex md:flex-wrap md:items-center md:justify-start md:gap-[30px]">
-  //       {projectsInfo.map(function (project) {
-  //         return (
-  //           <div
-  //             key={project.key}
-  //             className="inline-block relative max-w-[650px]"
-  //           >
-  //             <img
-  //               src={`src/projects/${project.image}`}
-  //               alt={project.alt}
-  //               className="h-auto w-[70vw] object-contain border-[1px] border-slate-900 md:h-[300px] md:w-auto"
-  //             />
-  //             <div
-  //               className="flex flex-col items-center justify-center w-full h-full absolute top-0 left-0 pt-3
-  //                         text-center transition-opacity duration-500 opacity-0 hover:opacity-100 focus-within:opacity-100
-  //                         bg-white bg-opacity-90 border-[1px] border-slate-900"
-  //             >
-  //               <h4 className="text-2xl pb-4">{project.title}</h4>
-  //               <br/>
-  //               <p className="px-3">{project.description}</p>
-  //               <br/>
-  //               <a className="p-2 mx-4" target="_blank" href={project.link}>
-  //                 <FontAwesomeIcon className="p-2 mx-4 text-4xl focus-within:text-blue-300 md: hover:text-blue-300 md:focus-within:text-white" icon={faSquareGithub} />
-  //               </a>
-  //               <a className=" focus-within:text-blue-300 md: hover:text-blue-300 md:focus-within:text-white" href="https://2025-sql-seagulls-blog.dev.io-academy.uk/">View project here</a>
-  //             </div>
-  //           </div>
-  //         );
-  //       })}
-  //     </div>
-  //   );
-  // }
-    
+}    
