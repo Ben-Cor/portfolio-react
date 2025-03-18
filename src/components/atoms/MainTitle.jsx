78f0d08febd977a5c5aabf667caa9aab60505829
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-03-18 09:58:14 +0000

    into changes

--- src/components/atoms/MainTitle.jsx
+++ src/components/atoms/MainTitle.jsx
@@ -3,7 +3,7 @@ export default function MainTitle({title, secondTitle}) {
 
     return(
         <div>
-            <h1 className="text-[2rem] font-primary md:text-[3rem]">{title}</h1>
+            <h1 className="text-[2rem] font-primary md:text-[5rem]">{title}</h1>
         </div>
     )
 }
