0f9cc23827cf6da5175424390589922c8bc80095
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-03-18 10:42:32 +0000

    text sizing adjusted

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,36 +7,38 @@ import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 export default function Intro() {
     return (
-        <div className="flex flex-col justify-center items-center text-center gap-4 px-10 h-screen">
-            <Images className="w-[20vw] min-w-[200px] max-w-[450px]" src={avatar} alt={"Animated style image of Ben Cornell"} />
-            <MainTitle title='Hi! I am Ben Cornell.' />
+        <div className="flex flex-col justify-center items-center text-center px-10 h-screen gap-8">
+            <Images className="w-[20vw] min-w-[200px] max-w-[450px] mb-4" src={avatar} alt={"Animated style image of Ben Cornell"} />
+            <div>
+                <MainTitle title='Hi! I am Ben Cornell.' />
+                <DecryptedText 
+                    text="I code things." 
+                    speed={100} 
+                    maxIterations={20} 
+                    characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+" 
+                    sequential="true"
+                    className="revealed text-[2rem] font-primary text-slate-900 lg:text-[4.5rem]" 
+                    parentClassName="all-letters"
+                    encryptedClassName="encrypted text-[2rem] font-primary text-slate-900 lg:text-[4.5rem]"
+                    animateOn="view"
+                    revealDirection="start"
+                    delay={300}
+                />
+            </div>
             <DecryptedText 
-                text="I code things." 
-                speed={100} 
-                maxIterations={20} 
-                characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+" 
-                sequential="true"
-                className="revealed text-[2rem] font-primary text-slate-900 lg:text-[5rem]" 
-                parentClassName="all-letters"
-                encryptedClassName="encrypted text-[2rem] font-primary text-slate-900 lg:text-[5rem]"
-                animateOn="view"
-                revealDirection="start"
-                delay={300}
+            text="React | PHP | MySQL" 
+            speed={75} 
+            maxIterations={20} 
+            characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+" 
+            sequential="true"
+            className="revealed text-[1.5rem] font-primary text-blue-700 md:text-[2rem]" 
+            parentClassName="all-letters"
+            encryptedClassName="encrypted text-[1.5rem] text-blue-700 font-primary md:text-[2rem]"
+            animateOn="view"
+            revealDirection="start"
+            delay={300}
             />
-              <DecryptedText 
-                text="React | PHP | MySQL" 
-                speed={75} 
-                maxIterations={20} 
-                characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+" 
-                sequential="true"
-                className="revealed text-[1.5rem] font-primary text-blue-700 md:text-[2rem]" 
-                parentClassName="all-letters"
-                encryptedClassName="encrypted text-[1.5rem] text-blue-700 font-primary md:text-[2rem]"
-                animateOn="view"
-                revealDirection="start"
-                delay={300}
-            />
-            <p className="text-[1.25rem] font-primary text-slate-900 lg:text-[2rem] pt-[5vh]">A little more about me</p>
+            <p className="text-[1rem] font-primary text-slate-900 lg:text-[1.5rem] pt-[5vh]">A little more about me</p>
             <FontAwesomeIcon className="p-2 mx-4 text-4xl hover:text-blue-700 duration-200" icon={faArrowDown} />
         </div>
     )
