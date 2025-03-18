8d8f8aa19f7998d411aed8e4c19dc9e37a7689cd
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-03-18 10:56:10 +0000

    intro spacing adjusted

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,8 +7,8 @@ import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 export default function Intro() {
     return (
-        <div className="flex flex-col justify-center items-center text-center px-10 h-screen gap-8">
-            <Images className="w-[20vw] min-w-[200px] max-w-[450px] mb-4" src={avatar} alt={"Animated style image of Ben Cornell"} />
+        <div className="flex flex-col justify-center items-center text-center px-10 h-screen gap-8 md:gap-4">
+            <Images className="w-[20vw] min-w-[200px] max-w-[400px] mb-4" src={avatar} alt={"Animated style image of Ben Cornell"} />
             <div>
                 <MainTitle title='Hi! I am Ben Cornell.' />
                 <DecryptedText 
